be26603b99ad056009fe4950018657aa7de5adde
khantuevao <khantuevao@gmail.com>
2022-06-21 16:32:46 +0200

    Make home.js function to populate home tab

---
+++ src/home.js
@@ -0,0 +1,49 @@
+export default populateHome();
+
+function populateHome() {
+  const box = document.createElement('div');
+  box.classList.add('box');
+
+  const title = document.createElement('div');
+  title.classList.add('title');
+  const titleTitle = document.createElement('h1');
+  titleTitle.textContent = 'Palo Alto Smoothie Bar';
+  title.appendChild(titleTitle);
+
+  box.appendChild(title);
+
+  const info = document.createElement('div');
+  info.classList.add('divInBox');
+  const infoText = document.createElement('p');
+  infoText.textContent = 'Every wellness journey is different. Ours started in 1971, pioneering a national shift away from prescription pads and towards a more holistic approach to wellbeing. Since then, we\'ve been known for nourishing the most health-obsessed humans on the planet.'
+  info.appendChild(infoText);
+  const infoTitle = document.createElement('h2');
+  infoTitle.textContent = 'Health-obsessed since 1971';
+  info.appendChild(infoTitle);
+
+  box.appendChild(info);
+
+  const hours = document.createElement('div');
+  hours.classList.add('divInBox');
+  const hoursTitle = document.createElement('h2');
+  hoursTitle.textContent = 'Hours';
+  hours.appendChild(hoursTitle);
+  const hoursText = document.createElement('p');
+  hoursText.innerHTML = 'Sunday: 8am - 8pm<hr>Monday: 6am - 6pm<hr>Tuesday: 6am - 6pm<hr>Wednesday: 6am - 6pm<hr>Thursday: 6am - 10pm<hr>Friday: 6am - 10pm<hr>Saturday: 8am - 10pm';
+  hours.appendChild(hoursText);
+
+  box.appendChild(hours);
+
+  const location = document.createElement('div');
+  location.classList.add('divInBox');
+  const locationTitle = document.createElement('h2');
+  locationTitle.textContent = 'Location';
+  location.appendChild(locationTitle);
+  const locationText = document.createElement('p');
+  locationText.textContent = '440 Portage Ave, Palo Alto, California 94306, USA';
+  location.appendChild(locationText);
+
+  box.appendChild(location);
+  
+  return box;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import './style.css';
+import populateHome from './home.js'
 
+
+
+const home = document.getElementById('home');
+const menu = document.getElementById('menu');
+const about = document.getElementById('about');
 const contentDiv = document.querySelector('#content');
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.getElementById('home').click();
+});
+
+function clear() {
+  contentDiv.innerHTML = '';
+};
+
+home.addEventListener('click', () => {
+  clear();
+  home.style.backgroundColor = 'skyblue';
+  menu.style.backgroundColor = '';
+  about.style.backgroundColor = '';
+  contentDiv.appendChild(populateHome);
+});
+
+menu.addEventListener('click', () => {
+  clear();
+  home.style.backgroundColor = '';
+  menu.style.backgroundColor = 'skyblue';
+  about.style.backgroundColor = '';
+  populateMenu;
+});
+
+about.addEventListener('click', () => {
+  clear();
+  home.style.backgroundColor = '';
+  menu.style.backgroundColor = '';
+  about.style.backgroundColor = 'skyblue';
+  populateAbout;
+});
+
+
+
 
 
 
